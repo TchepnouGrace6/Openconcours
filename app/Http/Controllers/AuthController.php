1b62fab33c1b84d60d1b7a988ea31a356d107c5b
TchepnouGrace6 <grace6tchepnou@gmail.com>
2026-03-21 14:54:08 +0100

    changement du controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,7 +10,7 @@ use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
 {
-    // Inscription candidat au concours
+    // Inscription candidat
     public function register(Request $request)
     {
         $request->validate([
